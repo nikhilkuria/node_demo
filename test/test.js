2c9d6735d90b6f8c0a85b49c8f9ed3e1e55407b8
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2016-09-03 00:04:54 +0530

    adding tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,96 @@
-var assert = require('assert');
+var assert = require('assert'),
+    app = require('../app.js'),
+    request = require('supertest');
 
-it ('doesn\'t do anything',function(){
-console.log(assert.equal(true,true,'assertion success'));
+describe('Basic Tests ', function(){
+  var user;
+  //try to get users and should get an empty listen
+  it('get users list',function(done){
+    request(app)
+      .get('/users')
+      .expect(200)
+      .end(function(err,res){
+        if(err){
+          return done(err);
+        }
+          var result = res.body;
+
+          assert.equal(result.success,true);
+
+          done();
+
+      });
+  });
+
+  //add an user and it should return the newly created user
+  it('should add an user',function(done){
+    request(app)
+      .post('/users')
+      .send({name:'matt'})
+      .expect(200)
+      .end(function(err,res){
+        if(err){
+          return done(err);
+        }
+
+        var result = res.body;
+
+        assert.equal(result.user.name,'matt');
+        assert(result.user.id>0);
+
+        done();
+      })
+  });
+  //add another user
+  it('should add an user',function(done){
+    request(app)
+      .post('/users')
+      .send({name:'jeff'})
+      .expect(200)
+      .end(function(err,res){
+        if(err){
+          return done(err);
+        }
+
+        var result = res.body;
+
+        assert.equal(result.user.name,'jeff');
+        assert(result.user.id>0);
+        user = result.user;
+        done();
+      })
+  });
+  //get users list and should return two items
+  it('get users list',function(done){
+    request(app)
+      .get('/users')
+      .expect(200)
+      .end(function(err,res){
+        if(err){
+          return done(err);
+        }
+          var result = res.body;
+
+          assert.equal(result.success,true);
+          assert.equal(result.users.length,2)
+          done();
+
+      });
+  });
+  //get second user
+  it('get second user',function(done){
+    var id = user.id;
+    request(app)
+      .get('/users/'+id)
+      .expect(200)
+      .end(function(err,res){
+        if(err){
+          return done(err);
+        }
+          var resultUser = res.body.user;
+          assert.equal(resultUser.name,user.name);
+          assert.equal(resultUser.id,user.id);
+          done();
+      })
+  })
 });
