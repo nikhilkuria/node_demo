2191b80e37f7acf7e0f9dbd44d669c9eab9039fc
Nikhil Kuriakose <nikhilkuria@hotmail.com>
2016-12-09 11:28:58 +0000

    adding more emitter demos

---
+++ events/emitterDemoScopes.js
@@ -0,0 +1,36 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+
+function Emitter(){
+    var self = this;
+    //closure to pass value 
+    //'nikhil' passed as param
+    setTimeout(function(){
+        self.emit('rain', 'nikhil');
+    },1000);
+    
+    setTimeout(function(){
+        self.emit('snow', 'neha');
+    },1000);
+}
+
+util.inherits(Emitter, EventEmitter);
+
+var emitter = new Emitter();
+
+//use on to set a regualar event
+emitter.on('rain', function(author){
+    console.log('When rain comes down, umbrellas go up : '+author);
+    //console.log(this);
+});
+
+emitter.once('snow', function(author){
+    console.log('When snow comes down, run for shelter : '+author);
+    //console.log(this);
+});
+
+console.log('calling sync event');
+
+emitter.emit('rain', 'nikhil');
+emitter.emit('snow', 'nikhil');
---
+++ events/errorEmitter.js
@@ -0,0 +1,23 @@
+var EventEmitter = require('events');
+var util = require('util');
+
+function ErrorEmitter(){
+    
+}
+
+util.inherits(ErrorEmitter, EventEmitter);
+
+var errorEmitter = new ErrorEmitter();
+
+//all errors are caught here
+errorEmitter.on('error', function(err){
+     console.log(err);
+});
+
+//catch uncaughtexception
+errorEmitter.once('uncaughtException', function(err){
+    console.log(err);
+    console.log('Inside uncaughtException');
+})
+
+errorEmitter.emit('error', new Error('man made error'));
